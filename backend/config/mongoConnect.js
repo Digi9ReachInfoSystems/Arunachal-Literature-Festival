645167556f5a7d7033ae43c32e4dfd3441882012
Uttam Shetty <uttamshetty126@gmail.com>
2025-07-04 15:57:42 +0530

    FIxxedBufferingBeforeQuery

--- backend/config/mongoConnect.js
+++ backend/config/mongoConnect.js
@@ -6,8 +6,8 @@ dotenv.config();
 const connectDB = async ()=>{
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-          
-        
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
             socketTimeoutMS: 45000,
             serverSelectionTimeoutMS: 30000,
         });
