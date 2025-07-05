c30e69c871d36f4b1f7c9a23cc6d713e14bc347a
Uttam Shetty <uttamshetty126@gmail.com>
2025-07-05 12:05:38 +0530

    COrsChanegd

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   "http://localhost:3000",
   "http://192.168.1.21:3000",
+  "https://arunachalwebapp.vercel.app",
   process.env.FRONTEND_URL || "https://arunachalwebapp.vercel.app",
  
 ];
